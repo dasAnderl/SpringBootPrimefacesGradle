b7b8c793ca9f00ec7f059ada0287220de4038219
dasanderl <an.anderl@gmail.com>
2014-09-07 13:02:51 +0200

    introduced abstractEntity

--- src/main/java/com/anderl/domain/TestEntity.java
+++ src/main/java/com/anderl/domain/TestEntity.java
@@ -1,32 +1,15 @@
 package com.anderl.domain;
 
-import org.springframework.boot.orm.jpa.EntityScan;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  * Created by dasanderl on 07.09.14.
  */
 @Entity
-public class TestEntity {
-
-   @Id
-   @GeneratedValue(strategy= GenerationType.AUTO)
-   private long id;
+public class TestEntity extends _AbstractEntity {
 
     private String name;
     private int age;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
