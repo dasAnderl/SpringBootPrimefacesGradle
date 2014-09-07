4353e4c9a72be74be5097f8c6511d9f87d29fa9e
dasanderl <an.anderl@gmail.com>
2014-09-07 13:01:47 +0200

    small bugfix

--- src/main/java/com/anderl/Application.java
+++ src/main/java/com/anderl/Application.java
@@ -3,21 +3,15 @@ package com.anderl;
 import com.anderl.helper.ViewScope;
 import com.google.common.collect.Maps;
 import com.sun.faces.config.ConfigureListener;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.jsf.el.SpringBeanFacesELResolver;
 import org.springframework.web.servlet.ViewResolver;
@@ -26,15 +20,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import javax.el.ELResolver;
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-//@EnableTransactionManagement
-//@EnableJpaRepositories("com.anderl.dao")
-//@EntityScan("com.anderl.domain")
 public class Application extends SpringBootServletInitializer implements ServletContextAware {
 
     public static void main(String[] args) {
@@ -51,7 +41,7 @@ public class Application extends SpringBootServletInitializer implements Servlet
 
     @Bean
     public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<ConfigureListener>(
+        return new ServletListenerRegistrationBean<>(
                 new ConfigureListener());
     }
 
@@ -69,7 +59,7 @@ public class Application extends SpringBootServletInitializer implements Servlet
     }
 
     @Bean
-    public CustomScopeConfigurer getViewScopeConfigurer() {
+    public static CustomScopeConfigurer getViewScopeConfigurer() {
         CustomScopeConfigurer customScopeConfigurer = new CustomScopeConfigurer();
         Map<String,Object> view = Maps.newHashMap();
         view.put("view", new ViewScope());
