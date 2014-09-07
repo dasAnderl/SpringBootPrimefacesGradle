30c026f309eadc04cadf2fb5b61b32153359489d
dasanderl <an.anderl@gmail.com>
2014-09-07 21:57:42 +0200

    added ilike criterion

--- src/main/java/com/anderl/hibernateext/Criteria.java
+++ src/main/java/com/anderl/hibernateext/Criteria.java
@@ -1,6 +1,7 @@
 package com.anderl.hibernateext;
 
 import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 
 /**
@@ -21,6 +22,11 @@ public enum Criteria {
     isNull {
         public <T> Criterion get(String property, T value) {
             return Restrictions.isNull(property);
+        }
+    },
+    like {
+        public <T> Criterion get(String property, T value) {
+            return Restrictions.ilike(property, (String) value, MatchMode.ANYWHERE);
         }
     };
 
--- src/test/java/com/anderl/service/HibernateCriterionServiceTest.groovy
+++ src/test/java/com/anderl/service/HibernateCriterionServiceTest.groovy
@@ -13,6 +13,10 @@ import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.transaction.annotation.Transactional
 
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.notNullValue
+import static org.junit.Assert.assertThat
+
 /**
  * Created by dasanderl on 07.09.14.
  */
@@ -26,36 +30,44 @@ class HibernateCriterionServiceTest extends groovy.util.GroovyTestCase {
     @Autowired
     TestEntityRepository testEntityRepository
 
+    String name1 = "name1"
+    String name2 = "name2"
+    String name3 = "name3"
+    int age1 = 1
+    int age2 = 2
+    int age3 = 3
+
+    TestEntity te1 = new TestEntity(name: name1, age: age1)
+    TestEntity te2 = new TestEntity(name: name2, age: age2)
+    TestEntity te3 = new TestEntity(name: name3, age: age3)
+
+    HibernateCriterion<Long, TestEntity> idCriterion = new HibernateCriterion<>(Long.class, TestEntity.class, "id", Criteria.eq, new Long(1));
+    HibernateCriterion<String, TestEntity> nameCriterion = new HibernateCriterion<>(String.class, TestEntity.class, "name", Criteria.eq, name1);
+    HibernateCriterion<Integer, TestEntity> ageCriterion = new HibernateCriterion<>(Integer.class, TestEntity.class, "age", Criteria.eq, age1);
+
+    HibernateCriterion<String, TestEntity> nameContainsCriterion = new HibernateCriterion<>(String.class, TestEntity.class, "name", Criteria.like, "name");
+
     @Before
     void before() {
-        System.out.println("saving new entity");
-        TestEntity entity = new TestEntity();
-        entity.setName("name1");
-        entity.setAge(1);
-        testEntityRepository.save(entity);
-        entity = new TestEntity();
-        entity.setName("name2");
-        entity.setAge(2);
-        testEntityRepository.save(entity);
-        entity = new TestEntity();
-        entity.setName("name3");
-        entity.setAge(3);
-        testEntityRepository.save(entity);
+        System.out.println("saving new entities");
+        testEntityRepository.save([te1, te2, te3]);
     }
 
     @Test
     @Transactional
     void testBuildCriteria() {
 
-        def entities = hibernateCriterionService.getEntities(getCriterions());
+        def entities = hibernateCriterionService.getEntities([idCriterion, nameCriterion, ageCriterion]);
+        assertThat(entities, notNullValue())
+        assertThat(entities.size(), is(1))
 
-        entities.each() { println " ${it}" }
-    }
+        def entity = entities.get(0)
+        assertThat(entity.name, is(name1))
+        assertThat entity.age, is(age1)
+        assertThat(entity.id, is(new Long(1)))
 
-    def getCriterions() {
-        def idCriterion = new HibernateCriterion<>(Long.class, TestEntity.class, "id", Criteria.eq, new Long(1));
-        def nameCriterion = new HibernateCriterion<>(String.class, TestEntity.class, "name", Criteria.eq, "name1");
-        def ageCriterion = new HibernateCriterion<>(Integer.class, TestEntity.class, "age", Criteria.eq, 1);
-        return [idCriterion, nameCriterion, ageCriterion]
+        entities = hibernateCriterionService.getEntities([nameContainsCriterion]);
+        assertThat(entities, notNullValue())
+        assertThat(entities.size(), is(3))
     }
 }
