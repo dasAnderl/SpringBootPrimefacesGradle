847d04f5b88aa26fbf01b9a9f01f8cfeab90f2fa
dasanderl <an.anderl@gmail.com>
2014-09-07 13:03:50 +0200

    added abstractentity

---
+++ src/main/java/com/anderl/domain/_AbstractEntity.java
@@ -0,0 +1,25 @@
+package com.anderl.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Created by dasanderl on 07.09.14.
+ */
+@MappedSuperclass
+public abstract class _AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
