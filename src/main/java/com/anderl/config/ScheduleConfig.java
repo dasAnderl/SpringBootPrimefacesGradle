0a934e62aa671202ad8daf5e5b218eeda6c32893
dasanderl <an.anderl@gmail.com>
2014-09-12 02:35:40 +0200

    commit for setting idea commit opions

--- src/main/java/com/anderl/config/ScheduleConfig.java
+++ src/main/java/com/anderl/config/ScheduleConfig.java
@@ -3,9 +3,6 @@ package com.anderl.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.Schedules;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 
 /**
  * Created by dasanderl on 12.09.14.
@@ -13,6 +10,7 @@ import org.springframework.security.core.userdetails.User;
 @Configuration
 @EnableScheduling
 public class ScheduleConfig {
+
 
     @Scheduled(fixedDelay = 10000)
     public void testScheduleTask() {
