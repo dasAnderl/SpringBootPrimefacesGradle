1e2a9b33e21e4369087b341c9fd2ac07e57ef88a
dasanderl <an.anderl@gmail.com>
2014-09-14 02:26:10 +0200

    added test dependencies jmockit added mockMvc test

---
+++ src/test/java/com/anderl/rest/TestEntityRestEndpointTest.java
@@ -0,0 +1,65 @@
+package com.anderl.rest;
+
+import com.anderl.dao.TestEntityRepository;
+import com.anderl.domain.TestEntityBuilder;
+import com.google.common.collect.Lists;
+import mockit.Injectable;
+import mockit.NonStrictExpectations;
+import mockit.Tested;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@FixMethodOrder
+public class TestEntityRestEndpointTest {
+
+    @Tested
+    TestEntityRestEndpoint testEntityRestEndpoint;
+
+    @Injectable
+    TestEntityRepository testEntityRepository;
+
+    private MockMvc mockMvc;
+
+    @Test
+    public void testFindAll() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(testEntityRestEndpoint).build();
+
+        long id = 9;
+        int age = 176765;
+        String name = "some name";
+        new NonStrictExpectations() {{
+            testEntityRepository.findAll();
+            result = Lists.newArrayList(
+                    TestEntityBuilder.aTestEntity()
+                            .withId(id)
+                            .withAge(age)
+                            .withName(name)
+                            .build());
+        }};
+
+        mockMvc.perform(get("/rest/testEntity/findAll"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+//                .andExpect(jsonPath("$[0].id", is(id)))
+                .andExpect(jsonPath("$[0].name", is(name)))
+                .andExpect(jsonPath("$[0].age", is(age)));
+    }
+
+    @Test
+    public void testGetByAgePath() throws Exception {
+
+    }
+
+    @Test
+    public void testAddTestEntity() throws Exception {
+
+    }
+}
