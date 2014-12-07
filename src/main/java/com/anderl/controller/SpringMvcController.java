48a0e2760461dacab704e01907923b3fad619ac8
dasanderl <an.anderl@gmail.com>
2014-12-07 20:59:54 +0100

    added thymeleaf

---
+++ src/main/java/com/anderl/controller/SpringMvcController.java
@@ -0,0 +1,22 @@
+package com.anderl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dasanderl on 07.12.14.
+ */
+@Controller
+public class SpringMvcController {
+
+    /**
+     * This will redirect to thymeleaf template /src/main/resources/templates/index.html
+     *
+     * @return
+     */
+    @RequestMapping("/thymeleaf")
+    String index() {
+        return "index";
+    }
+}
+
